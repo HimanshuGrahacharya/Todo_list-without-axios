21ab8489ca7f771c74e642ffc7aa265252fd00d1
Himanshu Shekhar Grahacharya <157715493+HimanshuGrahacharya@users.noreply.github.com>
2024-03-05 20:12:43 +0530

    Add files via upload

---
+++ src/components/Employee.js
@@ -0,0 +1,18 @@
+const Employee = [
+  {
+    id: "1",
+    Name: "Himanshu Grahacharya",
+    Age: "22",
+  },
+  {
+    id: "2",
+    Name: "Saroj Kumanr",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Sourav Kumanr",
+    Age: "24",
+  }
+]
+export default Employee;
